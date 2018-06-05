8fc89aae4994c4f5841e03d6cccf762c439c382e
MedsonF <37583804+MedsonF@users.noreply.github.com>
2018-06-05 07:41:13 -0300

    Avaliação Continuada P6
    Segue em anexo a avaliação que calcula a media entre os números digitados pelo usuário.

---
+++ P6.c
@@ -0,0 +1,45 @@
+# MTP-2018-1 //Medson José Alves da Silva Frazão Matrícula: 11721EAU015
+#include <stdio.h>
+
+int som(int num[], int a)
+{
+	int soma = 0, i;
+	for(i = 0; i < a; i++)
+	{
+		soma = soma + num[i];
+	}
+	return soma;
+}
+float divisao(float soma, float a)
+{
+	float media;
+	media = soma / a;
+	return media;
+}
+int main()
+{
+	int a, j, num[20], resultado;
+	printf("\nDigite a quantidade de valores (entre 5 e 20): ");
+	scanf("%d", &a);
+	getchar();
+	if(a >= 5 && a <= 20)
+	{
+	    for(j = 0; j < a; j++)
+	    {	
+		    printf("Valor %d: ", j+1);
+		    scanf("%d", &num[j]);
+		    getchar();
+	    }
+    }
+    else
+    {
+    	printf("Numero digitado invalido. ");
+	}
+	while(a >= 5 && a <= 20)
+	{
+	    resultado = som(num, a);
+	    printf("\nA Media entre os numeros digitados = %.2f", divisao(resultado, a));
+	    return 0;
+    }
+}
+
