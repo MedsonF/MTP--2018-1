ed46a381fd875a61ffb04b6147c59bff6e44dc01
MedsonF <37583804+MedsonF@users.noreply.github.com>
2018-06-05 07:51:36 -0300

    Avaliação Continuada P7.c

---
+++ P7.c
@@ -0,0 +1,31 @@
+# MTP-2018-1 //Medson José Alves da Silva Frazão Matríluca: 11721EAU015
+#include <stdio.h>
+
+int func(int m, int n)
+{
+	int i;
+	if(m == 0)
+	{
+		return n+1;
+    }
+    else if(m >0 && n == 0)
+    {
+    	return func(m-1, 1);
+	}
+	else if(m > 0 && n > 0)
+	{
+		return func(m-1, func(m, n-1));
+	}	
+}
+int main()
+{
+	int m, n;
+	printf("Digite o primeiro valor: ");
+	scanf("%d", &m);
+	getchar();
+	printf("Digite o segundo valor: ");
+	scanf("%d", &n);
+	getchar();
+    printf("\n\nResultado da funcao: %d", func(m,n));
+    return 0;
+}
